a605358112bd52d347383463005086995b1257bd
thr3eisl4nd <thre3islands@gmail.com>
2022-01-26 13:00:26 +0900

    商品登録機能の実装
    admin用のパスを設けるため、新しくコントローラーを作成
    アドミンユーザーの条件分岐記述
    READMEの記述修正

--- db/migrate/20220121105143_create_items.rb
+++ db/migrate/20220121105143_create_items.rb
@@ -6,6 +6,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :category_id, null: false
       t.integer :price,       null: false
       t.integer :stock,       null: false
+      t.references :user,     foreign_key: true
       t.timestamps
     end
   end

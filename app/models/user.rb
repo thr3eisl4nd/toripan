0bb73e8ad5c8b2f1aa95a9e0421a0b01786c36b5
thr3eisl4nd <thre3islands@gmail.com>
2022-02-04 11:35:20 +0900

    削除機能の追加
    トップページにマップも追加
    ユーザー登録機能の単体テストコードの実装(残りの機能も随時行う)
    テストコード時にバリデーションに抜けがあったので修正
    Basic認証の導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,10 @@ class User < ApplicationRecord
   has_many :messages
   has_many :comments
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i.freeze
   with_options presence: true do
     validates :nickname, format: { with: /\A[ぁ-ゔァ-ヴー－\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/, message: 'is invalid. Input full-width characters.' }
+    validates :password, format: { with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers.' }
     validates :family_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width character.' }
     validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width character.' }
     validates :phonenumber, format: { with: /\A[0-9]{10,11}\z/, message: 'Input only number.' }
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname              { "フェイカー" }
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    family_name           { "偽" }
+    first_name            { "物" }
+    phonenumber           { "0#{rand(0..9)}0#{rand(1_000_000..99_999_999)}" }
   end
 end
